d660b547b79c2736bddabd8ae424dd5e4bed9c2b
RadhaWagh <radhaneshwagh788@gmail.com>
2026-08-10 23:38:19 +0530

    feat: add destination images

--- src/components/home/PopularDestinations.jsx
+++ src/components/home/PopularDestinations.jsx
@@ -1,33 +1,40 @@
+import Bali from "../../assets/images/Bali.jpg";
+import Dubai from "../../assets/images/Dubai.jpg";
+import Goa from "../../assets/images/Goa.jpg";
+import Paris from "../../assets/images/Paris.jpg";
+import SwissAlps from "../../assets/images/SwissAlps.jpg";
+import Tokyo from "../../assets/images/Tokyo.jpg";
+
 const destinations = [
     {
         name: "Goa",
         country: "India",
-        image: "🏖️"
+        image: Goa
     },
     {
         name: "Paris",
         country: "France",
-        image: "🗼",
+        image: Paris
     },
     {
         name: "Bali",
         country: "Indonesia",
-        image: "🌴",
+        image: Bali
     },
     {
         name: "Dubai",
         country: "UAE",
-        image: "🏙️",
+        image: Dubai
     },
     {
         name: "Tokyo",
         country: "Japan",
-        image: "🗻",
+        image: Tokyo
     },
     {
         name: "Swiss Alps",
         country: "Switzerland",
-        image: "🏔️",
+        image: SwissAlps
     },
 ];
 function PopularDestinations() {
@@ -45,9 +52,13 @@ function PopularDestinations() {
                 {destinations.map((destination) => (
                 <div className="bg-gray-800/70 backdrop-blur-md rounded-2xl shadow-lg p-6 text-centre hover:scale-105 hover:bg-gray-700 transition duration-300 cursor-pointer">
 
-                    <div className="text-8xl mb-6">
-                        {destination.image}
+                    <div className="mb-6">
+                    <img
+                        src={destination.image}
+                        alt={destination.name}
+                        className="w-full h-48 object-cover rounded-xl"/>
                     </div>
+
                     <h3 className="text-2xl font-bold text-white">
                         {destination.name}
                     </h3>
